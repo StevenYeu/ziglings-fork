d8c8cc52e789885d824895f4289db3ffd75e44d7
Steven Yeu <stevenhanhyeu@gmail.com>
2025-11-18 01:08:46 -0800

    fixed errors on 26 and 34

--- exercises/034_quiz4.zig
+++ exercises/034_quiz4.zig
@@ -9,7 +9,7 @@ const std = @import("std");
 
 const NumError = error{IllegalNumber};
 
-pub fn main() void {
+pub fn main() !void {
     var stdout = std.fs.File.stdout().writer(&.{});
 
     const my_num: u32 = getNumber() catch 42;
